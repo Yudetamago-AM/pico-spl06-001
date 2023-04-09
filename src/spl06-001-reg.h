4bdbb067c66329b0833652a5f8976ee99e20dab1
Yudetamago-AM <akio@rittai.org>
2023-04-09 15:12:38 +0900

    initial commit

---
+++ src/spl06-001-reg.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// Pressure Value
+// 24bit(3byte) 2's complement value
+#define BNO085_PRS_B2 0x00 // MSB
+#define BNO085_PRS_B1 0x01 // LSB
+#define BNO085_PRS_B0 0x02 // XLSB
+
+// Pressure Config
+#define BNO085_PRS_CFG
+
+// Temperature Value
+// 24bit(3byte) 2's complement value
+#define BNO085_TMP_B2 0x03 // MSB
+#define BNO085_TMP_B1 0x04 // LSB
+#define BNO085_TMP_B0 0x05 // XLSB
+

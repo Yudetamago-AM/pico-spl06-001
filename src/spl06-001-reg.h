dc75fa910b1533e211ecfee247b8d056ef35e3d3
Yudetamago-AM <akio@rittai.org>
2023-04-09 15:17:17 +0900

    update regs

--- src/spl06-001-reg.h
+++ src/spl06-001-reg.h
@@ -1,17 +1,29 @@
 #pragma once
 
 // Pressure Value
-// 24bit(3byte) 2's complement value
-#define BNO085_PRS_B2 0x00 // MSB
-#define BNO085_PRS_B1 0x01 // LSB
-#define BNO085_PRS_B0 0x02 // XLSB
+// 24bit(3bytes) 2's complement value
+#define SPL06_PRS_B2 0x00 // MSB
+#define SPL06_PRS_B1 0x01 // LSB
+#define SPL06_PRS_B0 0x02 // XLSB
 
 // Pressure Config
-#define BNO085_PRS_CFG
+// 7   X
+// 6:4 PM_RATE(2bytes)
+// 3:0 PM_PRC (3bytes)
+#define SPL06_PRS_CFG 0x06 
 
 // Temperature Value
-// 24bit(3byte) 2's complement value
-#define BNO085_TMP_B2 0x03 // MSB
-#define BNO085_TMP_B1 0x04 // LSB
-#define BNO085_TMP_B0 0x05 // XLSB
+// 24bit(3bytes) 2's complement value
+#define SPL06_TMP_B2 0x03 // MSB
+#define SPL06_TMP_B1 0x04 // LSB
+#define SPL06_TMP_B0 0x05 // XLSB
+
+// Temperature Config
+// 7   TMP_EXT(1byte)
+// 6:4 TMP_RATE(2bytes)
+// 3   X
+// 2:0 TMP_PRC(3bytes)
+#define SPL06_TMP_CFG 0x07
+
+// Sensor Operating Mode and 
 
